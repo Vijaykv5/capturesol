a3120dccfd490ae93c7faeafb0c70b8eceab256a
vijay <vijaykv2228@gmail.com>
2024-11-24 17:11:34 +0530

    update best config file again

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  cssModules: true,
 };
  
 module.exports = nextConfig
